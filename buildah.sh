91ed83d8dbdb46ea82dc5eba435a3c86acac8206
gpeavy <gpv@alum.mit.edu>
2022-09-22 12:19:11 -0700

    Update buildah.sh

--- buildah.sh
+++ buildah.sh
@@ -3,15 +3,15 @@
 set -o errexit
 
 base_os="alpine"
-base_os_version="3.12.1"
-catt_version="v0.11.2"
+base_os_version="3.16.2"
+catt_version="v0.12.9"
 container=$(buildah from $base_os:$base_os_version)
-image_name="datmanslo/catt"
+image_name="gpeavy04/catt"
 
 # Labels
-buildah config --label maintainer="datmanslo <datmanslo@yahoo.com>" $container
+buildah config --label maintainer="garrett.peavy <garrett.peavy@gmail.com>" $container
 buildah config --label version=$catt_version $container
-buildah config --label repo="github.com/datmanslo/docker-catt" $container
+buildah config --label repo="github.com/gpeavy/docker-catt" $container
 
 buildah run $container apk add --no-cache python3 py3-pip
 buildah run $container python3 -m pip install catt==$catt_version
